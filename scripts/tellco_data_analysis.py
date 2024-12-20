f59d888fe6214528b3c96b0775e34813a54cbac4
abrehamashebir <abrisha96@gmail.com>
2024-12-20 10:09:06 +0300

    Modularizing data

--- scripts/tellco_data_analysis.py
+++ scripts/tellco_data_analysis.py
@@ -3,5 +3,38 @@ import pandas as pd
 import numpy as np
 
 
-def clean_data(data):
-    pass
+def fill_data_by_mean(data):
+    data.fillna(data.mean(numeric_only =True), inplace = True)
+
+    return data
+
+def drop_duplicate_data(data):
+    data =data.drop_duplicates()
+    
+
+def format_data(data):
+
+    data = data.rename(columns={
+        'Start':'Start Date',
+        'End':'End Date'
+    })
+
+    if 'date_column' in data.columns:
+        data['date_column'] = pd.to_datetime(data['date_column'], errors='coerce')
+
+    return data
+
+def treat_outlier_with_mean(data, column):
+    
+    Q1 = data[column].quantile(0.25)
+    Q3 = data[column].quantile(0.75)
+    IQR =Q3-Q1
+    lower_bound = Q1- 1.5*IQR 
+    upper_bound = Q3 + 1.5*IQR
+
+    data = data[~((data[column] < lower_bound) | (data[column]) > upper_bound)]
+    # data.drop(data[(data[column] < lower_bound) | (data[column] > upper_bound)].index, inplace=True)
+    
+    return data
+
+    return data
